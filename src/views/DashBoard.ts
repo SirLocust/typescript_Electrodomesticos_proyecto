5e0a2a3bab15bab8d041ed979bd69ca22723564f
sirlocust <jua56yu@hotmail.com>
2021-09-06 23:16:06 -0500

    se agregan estilos

--- src/views/DashBoard.ts
+++ src/views/DashBoard.ts
@@ -1,5 +1,8 @@
+import { Fridge } from '../models/Fridge';
 import { HouseholdAppliance } from '../models/HouseholdAppliance';
 import { ParseData } from '../models/ParseData';
+import { TV } from '../models/TV';
+import { TypeHouseHoldAppliace } from '../types/typeHouseHoldAppliance';
 
 export class DashBoard {
   private parseData: ParseData;
@@ -8,20 +11,64 @@ export class DashBoard {
   }
   public createTable(list: HouseholdAppliance[]) {
     this.checkIsDiv();
-    const div = document.createElement('div');
+    const div = document.createElement('table');
     div.setAttribute('id', 'dashboard');
-    const itemLeyenda = `<div><span>Tipo Electrodomestico</span><span>Consumo</span><span>Origen</span></div>`;
+    div.classList.add('table');
+    const itemLeyenda = `  <thead>
+    <tr>
+      <th scope="col">Tipo De electrodomestico</th>
+      <th scope="col">Consumo</th>
+      <th scope="col">Origen</th>
+      <th scope="col">Price</th>
+      <th scope="col">pulgadas/litros</th>
+      <th scope="col">tiene TDT</th>
+
+    </tr>
+  </thead>`;
     div.insertAdjacentHTML('beforeend', itemLeyenda);
+    let tbody = document.createElement('tbody') as HTMLElement;
+
     for (const item of list) {
-      const data = `<div >
-      <span>${this.parseData.requireStringTypeHousehold(
+      let arr = [];
+      if (item.typeHouseholdAppliance === TypeHouseHoldAppliace.TV) {
+        let itemN = item as TV;
+        arr.push(itemN.size);
+        arr.push(itemN.isTDT);
+      }
+      if (item.typeHouseholdAppliance === TypeHouseHoldAppliace.FRIDGE) {
+        let itemN = item as Fridge;
+        arr.push(itemN.capacity);
+      }
+      const data = `
+      <tr>
+      <th scope="row">${this.parseData.requireStringTypeHousehold(
         item.typeHouseholdAppliance
-      )}</span>
-              <span>${this.parseData.requireStringSpeding(item.spending)}</span>
-              <span>${this.parseData.requireStringOrigin(item.origin)}</span>
+      )}</th>
+      <td>${this.parseData.requireStringSpeding(item.spending)}</td>
+      <td>${this.parseData.requireStringOrigin(item.origin)}</td>
+      <td>${item.calculatePrice()}</td>
+      <td>${
+        item.typeHouseholdAppliance !== TypeHouseHoldAppliace.BASIC
+          ? arr[0]
+          : 'no tiene'
+      }</td>
+      <td>${
+        item.typeHouseholdAppliance !== TypeHouseHoldAppliace.BASIC && arr[1]
+          ? arr[1]
+          : 'no tiene'
+      }</td>
+
+
+    </tr>
+
+
+      <span></span>
+              <span></span>
+              <span></span>
               </div>`;
-      div.insertAdjacentHTML('beforeend', data);
+      tbody.insertAdjacentHTML('beforeend', data);
     }
+    div.insertAdjacentElement('beforeend', tbody);
     document.body.insertAdjacentElement('beforeend', div);
   }
 
@@ -37,10 +84,10 @@ export class DashBoard {
     let divTV = document.getElementById('divTv') as HTMLElement;
 
     let html = `<label for="">pulgadas</label>
-    <input type="number" id="sizeTv" />
+    <input class="form-control" type="number" id="sizeTv" />
     <label for="">tiene tdt</label>
 
-    <select name="options" id="isTdtSelect">
+    <select class="form-select" name="options" id="isTdtSelect">
       <option value="1">si</option>
       <option value="2">no</option>
     </select>`;
@@ -51,7 +98,7 @@ export class DashBoard {
     this.removeOptios();
     let divFridge = document.getElementById('divFridge') as HTMLElement;
     let html = `<label for="">capacidad Litros</label>
-    <input type="number" id="capacityFridge" />`;
+    <input class="form-control" type="number" id="capacityFridge" />`;
 
     divFridge?.insertAdjacentHTML('beforeend', html);
   }
